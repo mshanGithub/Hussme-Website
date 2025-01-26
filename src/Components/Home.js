d8cd09e04f9513def0bd0a902f4d30746c98bce8
GowthamSVAG <svagowtham3004@gmail.com>
2025-01-26 22:13:40 +0530

    play button

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,14 @@ import s1 from "../Components/Assets/Carousal/slide-1.jpg";
 import s2 from "../Components/Assets/Carousal/slide-2.jpg";
 import s3 from "../Components/Assets/Carousal/slide-3.jpg";
 import hand from "../Components/Assets/handshake.jpg";
-import c1 from "../Components/Assets/c1.jpg";
+import d1 from "../Components/Assets/Our Levers/digital.jpg";
+import d2 from "../Components/Assets/Our Levers/dig-back.png";
+import staff1 from "../Components/Assets/Our Levers/staff.jpg";
+import staff2 from "../Components/Assets/Our Levers/staff-back.png";
+import c1 from "../Components/Assets/Our Levers/consultation.jpg";
+import c2 from "../Components/Assets/Our Levers/cons-back.png";
+import play1 from "../Components/Assets/Our Levers/our-ser.png";
+import play2 from "../Components/Assets/Our Levers/our-ser-back.png";
 
 export function Home() {
   useEffect(() => {
@@ -75,45 +82,100 @@ export function Home() {
           <div className="img-container">
             <img src={hand} alt="" />
           </div>
-          <div className="container-2">
-            <div className="card">
-              <div className="default-content">
-                <div className="card-heading">
-                  <h2>Digital Marketing</h2>
+          <div className="containers">
+            <div className="container-1">
+              <div className="card">
+                <div className="default-content">
+                  <div className="card-heading">
+                    <h2>Digital Marketing</h2>
+                  </div>
+                  <div className="default-circle">
+                    <img src={d1} alt="" />
+                  </div>
                 </div>
-                <div className="default-circle">
-                  <img src={c1} alt="" />{" "}
-                </div>
-              </div>
-              <div className="hover-content">
-                <div className="hover-circle">
-                  <img src={c1} alt="" />{" "}
-                </div>
-                <div className="hover-describe"><h4>
-                  Enhance the website and utilize social platforms (Facebook,
-                  Instagram, X) for branding and generating leads for business
-                  growth.
-                  </h4>
+                <div className="hover-content">
+                  <div className="hover-heading">
+                    <h2>Digital Marketing</h2>
+                  </div>
+                  <div className="hover-circle">
+                    <img src={d2} alt="" />
+                  </div>
+                  <div className="hover-describe">
+                    <h4>
+                      Enhance the website and utilize social platforms
+                      (Facebook, Instagram, X) for branding and generating leads
+                      for business growth.
+                    </h4>
+                  </div>
                 </div>
               </div>
             </div>
 
-            <div className="card">
-              <div className="circle"></div>
-              <div className="content">Digital Marketing</div>
+            <div className="play-button">
+              <div className="play-button-inner">
+                <div className="play-button-front">
+                  <img src={play2} alt="Front" />
+                </div>
+                <div className="play-button-back">
+                  <img src={play1} alt="Back" />
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="play-button"></div>
-          <div className="container-1">
-            <div className="card">
-              <div className="circle"></div>
-              <div className="content">Digital Marketing</div>
+
+            <div className="container-2">
+              <div className="card">
+                <div className="default-content">
+                  <div className="card-heading">
+                    <h2>Staffing Services</h2>
+                  </div>
+                  <div className="default-circle">
+                    <img src={staff1} alt="" />
+                  </div>
+                </div>
+                <div className="hover-content">
+                  <div className="hover-heading">
+                    <h2>Staffing Services</h2>
+                  </div>
+                  <div className="hover-circle">
+                    <img src={staff2} alt="" />
+                  </div>
+                  <div className="hover-describe">
+                    <h4>
+                      Innovative on-demand staffing solutions - Hussme Hiring
+                      platform for business needs and flexible work schedule for
+                      talents
+                    </h4>
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="default-content">
+                  <div className="card-heading">
+                    <h2>Consultation</h2>
+                  </div>
+                  <div className="default-circle">
+                    <img src={c1} alt="" />
+                  </div>
+                </div>
+                <div className="hover-content">
+                  <div className="hover-heading">
+                    <h2>Consultation</h2>
+                  </div>
+                  <div className="hover-circle">
+                    <img src={c2} alt="" />
+                  </div>
+                  <div className="hover-describe">
+                    <h4>
+                      Streaming the business process for growth and bring in the
+                      out of the box solution to reduce expenses
+                    </h4>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      
-      
     </div>
   );
 }
