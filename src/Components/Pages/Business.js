9e70407629be8246b7b519e4b1881ff864d402dd
GowthamSVAG <svagowtham3004@gmail.com>
2025-02-15 16:54:44 +0530

    Business page responsive

--- src/Components/Pages/Business.js
+++ src/Components/Pages/Business.js
@@ -1,4 +1,5 @@
 import "./Business.css";
+import blogImg from "../Assets/svg/Blog-img-1.svg";
 import card1 from "../Assets/Business-page/card-1.png";
 import card2 from "../Assets/Business-page/card-2.png";
 import card3 from "../Assets/Business-page/card-3.png";
@@ -16,7 +17,7 @@ export function Business() {
           </div>
         </div>
         <div className="img-detail">
-          <div className="blog-img"> </div>
+          <div className="blog-img"> <img src={blogImg} alt="" /></div>
           <div className="blog-detail">
             <p>
               We partner with you to understand your business needs, providing
@@ -45,7 +46,6 @@ export function Business() {
           <div className="bus-card-img">
             <div className="card-img">
               <img src={card2} alt="" />
-
               <p>
                 Innovative on-demand staffing solutions through the Hussme
                 Hiring platform, connecting businesses with part-time talent as
@@ -61,7 +61,6 @@ export function Business() {
           <div className="bus-card-img">
             <div className="card-img">
               <img src={card3} alt="" />
-
               <p>
                 Enhance your website and leverage social platforms (Facebook,
                 Instagram, X) for branding and lead generation to drive business
