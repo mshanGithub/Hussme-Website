83ebd853572ef980c81a4cc1461116c661010db9
GowthamSVAG <svagowtham3004@gmail.com>
2025-02-18 16:40:17 +0530

    Remove unused image and enhance Talent page layout with new cards and responsive design

--- src/Components/Pages/Talent.js
+++ src/Components/Pages/Talent.js
@@ -1,8 +1,12 @@
 import "./Talent.css";
+import card1 from "../Assets/Talent-page/card-1.png";
+import card2 from "../Assets/Talent-page/card-2.png";
+import card3 from "../Assets/Talent-page/card-3.png";
 export function Talent() {
   return (
     <div className="talent-page">
       <div className="talent-title">Talent</div>
+      {/* Start of Talent intro Section */}
       <div className="tal-content">
         <div className="left-section">
           <div className="talent-slogan">
@@ -14,13 +18,60 @@ export function Talent() {
             <div className="tal-line-3">schedule</div>
           </div>
           <div className="tal-define">
-            Hussme offers a platform where you can set your own work schedule
-            for gigs. Businesses will send gig requests based on your
-            availability.
+            <p>
+              Hussme offers a platform where you can set your own work schedule
+              for gigs. Businesses will send gig requests based on your
+              availability.
+            </p>
           </div>
         </div>
         <div className="right-section"></div>
       </div>
+      {/* End of Talent intro Section */}
+      {/* Start of talent card Section */}
+
+      <div className="bus-tal-card-back">
+        <div className="bus-tal-card">
+          <div className="bus-card-img">
+            <div className="card-img">
+              <img src={card1} alt="" />
+
+              <p>
+                Hussme offers a platform where you can set your own work
+                schedule for gigs. Businesses will send gig requests based on
+                your availability.
+              </p>
+            </div>
+          </div>
+          <div className="static-card-title">Set up your own work schedule</div>
+        </div>
+        <div className="bus-tal-card">
+          <div className="bus-card-img">
+            <div className="card-img">
+              <img src={card2} alt="" />
+              <p>
+                Hussme offers online courses for training and guidance related
+                to work requests.
+              </p>
+            </div>
+          </div>
+          <div className="static-card-title">Online Training and Assitance</div>
+        </div>
+        <div className="bus-tal-card">
+          <div className="bus-card-img">
+            <div className="card-img">
+              <img src={card3} alt="" />
+              <p>
+                Payment settlements are processed within a day for quick and
+                direct deposit to your bank account, ensuring easy handling.
+              </p>
+            </div>
+          </div>
+          <div className="static-card-title">Tax Support</div>
+        </div>
+      </div>
+
+      {/* End of talent card Section*/}
     </div>
   );
 }
